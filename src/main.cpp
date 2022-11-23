d7ebc60bdadf150392b32e3c913f29fcf495358d
Brian Chau <brian.chau@outlook.com>
2022-11-23 13:51:10 -0800

    Added compiler flags to CMakeLists.txt, adjusted README to specify which C++ standard to use with VSCode

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,8 @@
 int main() {
     std::unique_ptr<Student> ptr(std::make_unique<Student>());
     std::binary_semaphore a{1};
-
+    // Uncomment this to verify that the compiler flags are enabled properly
+    // int b(0), c(0), d(0);
     ptr->SayHi();
     return 0;
 }
